4aca08578fd2f241ca1a27e89994df48d808fa2d
mahdi7985-glitch <mahdi7985@gmail.com>
2026-09-04 15:22:49 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -184,11 +184,7 @@ class CryptoSignalBot:
                         df=data['df'],
                         symbol=symbol,
                         current_price=data['price'],
-                        data_quality=(
-                            data['data_quality'].to_dict()
-                            if data.get('data_quality') is not None
-                            else None
-                                ),
+                        data_quality=data_quality=data.get('data_quality'),
                         higher_tf=data.get('higher_tf', {})  # 🔥 جدید
                     )
 
